da99ec1c66df36c23fbf3c9462aa09244a15cb80
Antonio Di Palma <dipalma.antonio98@gmail.com>
2024-08-26 16:09:13 +0200

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# shortest-path-finder
+# Shortest Path Finder
+
+🚀 This Python project is a shortest path finder that utilizes the Breadth-first search algorithm.
+
+## How it Works
+
+The Shortest Path Finder uses the Breadth-first search algorithm to find the shortest path between two nodes in a graph. 🧭 It explores all the neighboring nodes at the current level before moving on to the next level. This ensures that the shortest path is found efficiently. ⚡️
+
+## Usage
+
+To use this project, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Run the `shortest_path_finder.py` script.
+3. The program will display the shortest path between the two nodes.
+
+Feel free to customize the graph and explore different scenarios! 🎯
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). 📝
---
+++ path-finder.py
@@ -0,0 +1,94 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def print_maze(stdscr, maze, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row, in enumerate(maze):
+        for j, value in enumerate(row):
+            if (i, j) in path:
+                stdscr.addstr(i, j*2, "X", RED)
+            else:
+                stdscr.addstr(i, j*2, value, BLUE)
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return (i, j)
+    return None
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_position = find_start(maze, start)
+
+    q = queue.Queue()
+    q.put((start_position, [start_position]))
+
+    visited = set()
+
+    while not q.empty():
+        current_position, path = q.get()
+        row, col = current_position
+
+        stdscr.clear()
+        print_maze(stdscr, maze, path)
+        time.sleep(0.2)
+        stdscr.refresh()
+
+        if maze[row][col] == end:
+            return path
+
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0: # Up
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze): # Down
+        neighbors.append((row + 1, col))
+    if col > 0: # Left
+        neighbors.append((row, col - 1))
+    if col + 1 < len(maze[0]): # Right
+        neighbors.append((row, col + 1))
+    return neighbors
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    if maze == []:
+        raise ValueError("Maze is empty")
+    else:
+        find_path(maze, stdscr)
+        stdscr.getch()
+
+wrapper(main)
